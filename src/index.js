15c245ba0f1907f6ecf7edd27dee840a480b3f78
Denis I <denis.ignatovich.m@gmail.com>
2019-03-13 22:57:35 +0300

    add task implementation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const totalTriangles = preferences.reduce((acc, spichonee, i) => {
+    const aFakeIndex = i + 1;
+    const bRealIndex = spichonee - 1;
+    const cRealIndex = preferences[bRealIndex] - 1;
+    if (bRealIndex === cRealIndex) {
+      return acc;
+    }
+    const c = preferences[cRealIndex];
+    return c === aFakeIndex ? acc + 1: acc;
+  }, 0);
+  const uniqueTriangles = totalTriangles / 3;
+  return uniqueTriangles;
 };
